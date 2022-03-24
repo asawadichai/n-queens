b8fc0d69809612ef9ead73a201deb2556fd9e086
Gene_Yang <genehcyang@gmail.com>
2022-03-23 21:01:41 -0700

    WIP solvers.js

--- src/solvers.js
+++ src/solvers.js
@@ -12,11 +12,58 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
+/*
+I: n which represents number of rowsxcolumns of chess board
+O; nxn matrix chess board representing a valid solution for placed rooks that cannot attach each other
+E:
+C: n needs to be positive iteger
+Strategy:
+  1. create an empty board with n number of rows and columns (with helper function)
+  2. pass the empty board into the Board function so it gets all the methods
+  3. create a tree with the empty board
+  4. create children of the empty board with rooks placed in every possible location
+    a: while creating children, check for:
+      aa: check for conflicts
+      ab: set true/false for isValidSolution
+      ac: check if number of rooks placed equals to n
+    b: if there are conflicts, stop creating children
+    c: if isValidSolution is false, and number of rooks is less than n, try to create more children
+  5. once entire tree has been traversed, go into the tree and find all isValidSolution boards
+  6. put solution board in an array
+  7. remove duplicate solutions
+  8. return first valid solution, and return n number of element in solutions array
 
+Psuedo:
+*/
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board(makeEmptyBoard(n));
+  var pieces = 0;
+  var solutionTree = new Tree(board, n, pieces);
+
+  // function(board)
+  // set a "1" starting from first row first column
+  // iterate through n columns
+  // once we reach n columns, iterate through next row
+  // try to put a 1 in the current location. If there is already a 1, skip,
+  // otherwise, put a 1 in the location, and push entire array to a children's array.
+  // iterate through n columns
+  // next row
+  var childrensArray = [];
+  var possibleSolutions = function (board) {
+    for (var row = 0; row < n; row++) {
+      for (var col = 0; col < n; col++) {
+        board.attributes[row][col];
+      }
+    }
+
+  };
+  // BC: isValidSolution === true, n === pieces, there is a conflict
+  // RC: there are no conflicts, is valid solution === false, pieces < n
+
+
+  var solution = board; //fixme
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -45,3 +92,21 @@ window.countNQueensSolutions = function(n) {
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+window.makeEmptyBoard = function(n) {
+  var matrix = Array(n).fill(Array(n).fill(0));
+  return matrix;
+};
+
+window.Tree = function(inputBoard, n, pieces) {
+  this.board = inputBoard;
+  this.n = n;
+  this.pieces = 0;
+  this.isValidSolution = false;
+  this.children = [];
+};
+
+window.Tree.prototype.addChild = function(matrix) {
+  var newChild = window.Tree(matrix);
+}
+
